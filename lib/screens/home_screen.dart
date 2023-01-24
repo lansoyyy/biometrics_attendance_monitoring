e5d288eeae823b632fa0739fd56de380ca12b23d
Lans12345 <olanalans12345@gmail.com>
2023-01-24 19:43:01 +0800

    added list tab

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -80,12 +80,72 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ),
-            const Expanded(
+            Expanded(
               child: SizedBox(
                 child: TabBarView(children: [
-                  RegisterTab(),
-                  AttendanceTab(),
-                  Center(child: Text('3')),
+                  const RegisterTab(),
+                  const AttendanceTab(),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: TextBold(
+                            text: 'List of students',
+                            fontSize: 18,
+                            color: Colors.white),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+                        child: Container(
+                          height: 380,
+                          width: double.infinity,
+                          color: Colors.white,
+                          child: SingleChildScrollView(
+                            child: DataTable(columns: [
+                              DataColumn(
+                                  label: TextBold(
+                                      text: 'Name',
+                                      fontSize: 14,
+                                      color: Colors.black)),
+                              DataColumn(
+                                  label: TextBold(
+                                      text: 'Event',
+                                      fontSize: 14,
+                                      color: Colors.black)),
+                              DataColumn(
+                                  label: TextBold(
+                                      text: 'Date',
+                                      fontSize: 14,
+                                      color: Colors.black)),
+                            ], rows: [
+                              for (int i = 0; i < 50; i++)
+                                DataRow(cells: [
+                                  DataCell(
+                                    TextRegular(
+                                        text: 'John Doe',
+                                        fontSize: 12,
+                                        color: Colors.black),
+                                  ),
+                                  DataCell(
+                                    TextRegular(
+                                        text: 'Sports Day',
+                                        fontSize: 12,
+                                        color: Colors.black),
+                                  ),
+                                  DataCell(
+                                    TextRegular(
+                                        text: '01/20/23',
+                                        fontSize: 12,
+                                        color: Colors.black),
+                                  )
+                                ]),
+                            ]),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ]),
               ),
             ),
